d483c6f725e5729a65acfb69748b1d9ee899f462
Robert Ducusin <robert.ducusin@webeelabs.com.mt>
2025-07-02 00:43:47 +0800

    Add typecasting and variables demonstration scripts

---
+++ Variables/typecasting.py
@@ -0,0 +1,7 @@
+# Type casting = the process of converting a variable from one data type to another
+#                str(), int(), float(), bool()
+
+name = "Bro Code"
+age = 25
+gpa = 3.2
+is_student = True
---
+++ Variables/variables.py
@@ -0,0 +1,38 @@
+# This is my python program
+# print("I like pizza!")
+# print("It's really good!")
+
+# Variable = A container for a value (String, Integer, Float, Boolean)
+#            A variable behaves as if it was the value it contains 
+
+# Strings - are a series of characters that can include numbers but treated as characters. Strings are inside the quote (") or (')
+first_name = "Bro"
+food = "Mango Float"
+email = "bro123@fake.com"
+
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+# print(f"Your email is {email}")
+
+# Integers - are basically a numbers - these values should not have quotes in it
+age = 25
+quantity = 2
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float - a floating point number -- those who have decimal points
+price = 10.99
+gpa = 8.5
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"My gpa is: {gpa}")
+print(f"You ran {distance}km")
+
+# Boolean - it is a True or False value
+
+true = True
+false = False
